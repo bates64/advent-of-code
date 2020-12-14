1ee937d6614ddb2d033c4e500c9851e713feee7a
Alex Bates <hi@imalex.xyz>
2020-12-14 23:41:09 +0000

    day 13 part 2

--- src/day/day13.rs
+++ src/day/day13.rs
@@ -20,6 +20,41 @@ pub fn part1() -> Result<String, Error> {
 }
 
 pub fn part2() -> Result<String, Error> {
-    // idk lol
-    Err(Error::Unimplemented)
+    let buses: Vec<(usize, usize)> = INPUT.lines()
+        .skip(1)
+        .next().unwrap()
+        .split(',')
+        .enumerate()
+        .filter(|(_, s)| *s != "x")
+        .map(|(delta, s)| (delta, s.parse().unwrap()))
+        .collect();
+
+    let mut time = 0;
+    let mut stride = 1;
+
+    for (delta, id) in buses {
+        loop {
+            if (time + delta) % id == 0 {
+                stride = lcm(id, stride);
+                break;
+            }
+
+            time += stride;
+        }
+    }
+
+    Ok(time.to_string())
+}
+
+fn lcm(a: usize, b: usize) -> usize {
+    (a * b) / gcd(a, b)
+}
+
+fn gcd(mut a: usize, mut b: usize) -> usize {
+    while b != 0 {
+        let rem = a % b;
+        a = b;
+        b = rem;
+    }
+    a
 }
